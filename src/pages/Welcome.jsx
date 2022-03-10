732e0db3075a5a85c453967ec35b43bb9ac48de1
gonjila <lgonjila@gmail.com>
2022-03-10 18:47:44 +0400

    made welcome background

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,9 +1,116 @@
 import { Link } from "react-router-dom";
+import Particles from "react-tsparticles";
 import styled from "styled-components";
 
 function WelcomePage() {
   return (
     <Container className="page">
+      <Particles
+        options={{
+          particles: {
+            number: {
+              value: 500,
+              density: {
+                enable: true,
+                value_area: 790,
+              },
+            },
+            color: {
+              value: "#ffffff",
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#000000",
+              },
+              polygon: {
+                nb_sides: 5,
+              },
+            },
+            opacity: {
+              value: 0.8,
+              random: false,
+              anim: {
+                enable: true,
+                speed: 0.2,
+                opacity_min: 0,
+                sync: false,
+              },
+            },
+            size: {
+              value: 2,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 2,
+                size_min: 0,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: false,
+              distance: 150,
+              color: "#ffffff",
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 0.2,
+              direction: "none",
+              random: true,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
+          },
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: {
+                enable: true,
+                mode: "bubble",
+              },
+              onclick: {
+                enable: true,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                line_linked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 84,
+                size: 1,
+                duration: 3,
+                opacity: 1,
+                speed: 3,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+              push: {
+                particles_nb: 20,
+              },
+              remove: {
+                particles_nb: 15,
+              },
+            },
+          },
+        }}
+      />
       <h1>Welcome Rocketeer !</h1>
       <Link id="toPersonalInformation" to="/personal-information">
         <button className="redberryBtn">Start Questionnaire</button>
@@ -19,7 +126,8 @@ function WelcomePage() {
 export default WelcomePage;
 
 const Container = styled.div`
-  background: url("/images/bgStars.png") black center no-repeat;
+  /* background: url("/images/bgStars.png") black center no-repeat; */
+  background: black;
   overflow: hidden;
 
   display: flex;
